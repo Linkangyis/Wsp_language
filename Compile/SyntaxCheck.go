4a378d398141a271be09ce617c15711f3900a903
Linkin <59390570+Linkangyis@users.noreply.github.com>
2022-10-24 09:59:42 +0800

    Update SyntaxCheck.go

--- Compile/SyntaxCheck.go
+++ Compile/SyntaxCheck.go
@@ -21,7 +21,6 @@ func Check(Opcode map[int]map[int]Body_Struct_Run){
     for i:=0;i<=len(Opcode)-1;i++{
         for z:=0;z<=len(Opcode[i])-1;z++{
             if Opcode[i][z].Type==205&&z!=len(Opcode[i])-1{
-                fmt.Println(Opcode[i])
                 if Opcode[i][z+1].Type!=212 && Opcode[i][z+1].Type!=90&&(Opcode[i][z+1].Type!=91||(Opcode[i][z+1].Type==91&&Opcode[i][z+2].Type==98))&& Opcode[i][z+1].Type!=92&& Opcode[i][z+1].Type!=93&& Opcode[i][z+1].Type!=94{
                     Error("结尾缺少一个",Opcode[i][z].Line,";")
                 }
