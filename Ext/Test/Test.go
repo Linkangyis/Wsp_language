0f427ea4396742214c9314fb7f9d8ab6c05398ba
Linkin <1961520994@qq.com>
2022-11-10 19:38:59 +0800

    Update Test.go

--- Ext/Test/Test.go
+++ Ext/Test/Test.go
@@ -1,12 +1,13 @@
 package main
 
 import(
-  "fmt"
+    "fmt"
+    "Wsp/Module/Vm"
 )
 func Func_Info()(map[int]string){
     info := make(map[int]string)
-    info[0] = "Testb"
-    info[1] = "Tests"
+    info[0] = "ReturnArray"
+    info[1] = "ReadArray"
     return info
 }
 func Package_Info()(string){
@@ -15,13 +16,17 @@ func Package_Info()(string){
 }
 
 
-func Testb(Value map[int]string)(string){
-    fmt.Println(Value[0]+Value[1])
-    return "TRUE"
+func ReturnArray(Value map[int]string)(string){
+    ResMap := make(map[string]interface{})   //多维数组 1层
+    ResMap["1"] = make(map[string]interface{})    //多维数组二层
+    ResMap["0"] = "TestArray[0]"
+    ResMap["1"].(map[string]interface{})["2"] = "TestArray[1][2]"
+    return vm.NewArrayType(ResMap)    //写入RAM并获取引用指针
 }
 
-func Tests(Value map[int]string)(string){
-    fmt.Println(Value[0])
+func ReadArray(Value map[int]string)(string){
+    Array:=vm.ArrayRead(Value[0])   //输入引用指针，将指针转化为 map[string]interface{} 多维数组类型
+    fmt.Println(Array)
     return "TRUE"
 }
 
