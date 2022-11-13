5bbecb98a4f71377064606537858b75ec37392cb
Linkin <1961520994@qq.com>
2022-11-13 22:05:55 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import(
   "io/ioutil"
   "github.com/peterh/liner"
 )
-const Version = "4.6.0 BEAT 3"
+const Version = "4.6.0"
 func RunCode(Code string,FilesStruct vm.FileValue)vm.FileValue{
     Opcode:=Compile(Code)
     if ini.DebugsIf(){
