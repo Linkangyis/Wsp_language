f0b8ac4cb10c82b470b72095eb2dc60babf0f15b
Linkin <1961520994@qq.com>
2022-12-10 15:19:14 +0800

    Add files via upload

--- Module/Vm/VmUseListApi.go
+++ Module/Vm/VmUseListApi.go
@@ -25,9 +25,17 @@ func ArrayRead(Value string)map[string]interface{}{
     return res
 }
 
+func VarFree(varName string,VarValue *FileValue){
+    File:= So_Array_Stick(VarValue.FuncName+varName,VarValue)
+    Del_Dir(File+"/")
+    Del_Dir(File)
+    Del_File(File)
+    Del_Files(File)
+}
 
 func NewArrayType(Values map[string]interface{})string{
     RamS:=InitVar("RES",3,FileValue{})
+    VarFree("__NEWRES__",&RamS)
     AddArray("__NEWRES__[__NEWRES__]","<INIT>",&RamS)
     ResAId:=Read_Array("__NEWRES__",&RamS)
     Path:=Pointeref[ResAId]+"/"
